f46fa87d53d44f91cb5caa4d1b3db9d91a3e252f
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-07-23 02:47:05 -0700

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,15 @@
+public class odd {
+    public static void main(String[] args)
+{
+        if(n%2==0)
+        {
+          System.out.println("this is even number"+i);
+        }
+        else
+        {
+            System.out.println("this is odd"+i);
+        }
+            
+        }
+    }
+}
